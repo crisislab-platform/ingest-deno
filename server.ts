1a4b913cb04f43ffcdc364201b1f4a5acfa5c2dd
Hong <bhong@hpc-crisislab04.massey.ac.nz>
2023-02-04 17:55:53 +1300

    server changes

--- server.ts
+++ server.ts
@@ -5,6 +5,8 @@ import { sensorHandler, clientHandler } from "./connectionHandler.ts";
 async function reqHandler(request: Request) {
   const url = new URL(request.url);
   const pathname = url.pathname;
+  
+  console.log("request:", "'" + pathname + "'")
 
   // if (pathname == "/ingest/websocket") {
   //   return sensorHandler(request);
@@ -18,22 +20,24 @@ async function reqHandler(request: Request) {
 
     if (request.headers.get("Upgrade") != "websocket") {
       console.log("Upgrade header is not websocket");
-      return await serveFile(request, "./live-data-graphs/dist/index.html");
+      return await serveFile(request, "/home/b/bhong/ingest-deno2/live-data-graphs/dist/index.html");
     }
 
     return clientHandler(request);
-  }
+  } 
+  
+  if (pathname === "/") return await serveFile(request, "/home/b/bhong/ingest-deno2/live-data-graphs/dist/index.html");
 
-  return await serveFile(request, "./live-data-graphs/dist" + pathname);
+  return await serveFile(request, "/home/b/bhong/ingest-deno2/live-data-graphs/dist" + pathname);
 }
 
-fetch("https://internship-worker.benhong.workers.dev/api/v0/sensors/online", {
-  headers: {
-    authorization: "bearer eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlcyI6WyJzZW5zb3JzOm9ubGluZSJdLCJlbWFpbCI6ImluZ2VzdEBiZW5ob25nLm1lIiwibmFtZSI6IkxpdmUgRGF0YSBTZXJ2ZXIiLCJpYXQiOjE2NTczNDEzMjEuODY1LCJleHAiOjE2ODg4NzczMjEuODY1LCJpc3MiOiJodHRwczovL2NyaXNpc2xhYi5vcmcubnoiLCJhdWQiOlsiYWRtaW4iXX0=.9jaINkWZNNT3iMvq-XmsNVv4ARiEFkzZA8lD_2Uw2F6dXZ-EbwK1FVzDlG8AZLlozmOXtc6YX3O52u8Tm6oEiw"
-  },
-  method: "POST",
-  body: JSON.stringify({ all: true, timestamp: Date.now(), state: false })
-}).then((res) => { console.log(res.status); res.text().then(a => console.log(a)) }).catch((err) => { console.log(err); });
+//fetch("https://internship-worker.benhong.workers.dev/api/v0/sensors/online", {
+//  headers: {
+//    authorization: "bearer eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlcyI6WyJzZW5zb3JzOm9ubGluZSJdLCJlbWFpbCI6ImluZ2VzdEBiZW5ob25nLm1lIiwibmFtZSI6IkxpdmUgRGF0YSBTZXJ2ZXIiLCJpYXQiOjE2NTczNDEzMjEuODY1LCJleHAiOjE2ODg4NzczMjEuODY1LCJpc3MiOiJodHRwczovL2NyaXNpc2xhYi5vcmcubnoiLCJhdWQiOlsiYWRtaW4iXX0=.9jaINkWZNNT3iMvq-XmsNVv4ARiEFkzZA8lD_2Uw2F6dXZ-EbwK1FVzDlG8AZLlozmOXtc6YX3O52u8Tm6oEiw"
+//  },
+//  method: "POST",
+//  body: JSON.stringify({ all: true, timestamp: Date.now(), state: false })
+// }).then((res) => { console.log(res.status); res.text().then(a => console.log(a)) }).catch((err) => { console.log(err); });
 
 (async () => {
   const socket = await Deno.listenDatagram({
