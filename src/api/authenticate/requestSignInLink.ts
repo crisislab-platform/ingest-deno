c8c599a8be8cd54a418ef0a49df6c905beba27d8
Zade Viggers <zade@viggers.net>
2023-12-13 12:32:33 +1300

    Disable email sending for now

--- src/api/authenticate/requestSignInLink.ts
+++ src/api/authenticate/requestSignInLink.ts
@@ -1,4 +1,4 @@
-import { IRequest } from "itty-router";
+import { IRequest, error } from "itty-router";
 import createUserToken from "./createUserToken.ts";
 
 import {
@@ -26,72 +26,78 @@ const emails = {
 export default async function requestSignInLink(
 	request: IRequest
 ): Promise<Response> {
-	const {
-		email,
-		type = "welcome",
-	}: { email?: string; type?: keyof typeof emails } = request.params || {};
-
-	if (!email) {
-		return new Response("Invalid", { status: 404 });
-	}
-
-	const user = await getUserByEmail(email);
-
-	if (!user) {
-		return new Response("User not found", { status: 404 });
-	}
-
-	const token = await createUserToken(email, ["admin"], 7 * 24 * 60 * 60);
-
-	const url = new URL(request.url);
-
-	const emailTemplate = emails[type] || emails["sign-in"];
-
-	const content = emailTemplate.content(
-		`https://admin.crisislab.org.nz/token-sign-in?token=${token}&type=${type}${
-			url.searchParams.get("return_to")
-				? `&return_to=${url.searchParams.get("return_to")}`
-				: ""
-		}`,
-		user.name
+	// TODO: Re-implement email sending
+	return error(
+		503,
+		"Sign-in via link is currently disabled. Ask Zade to reset your password."
 	);
 
-	const emailPayload = {
-		personalizations: [
-			{
-				to: [{ email, name: user.name }],
-				dkim_domain: Deno.env.get("DKIM_DOMAIN"),
-				dkim_selector: Deno.env.get(" DKIM_SELECTOR"),
-				dkim_private_key: Deno.env.get(" DKIM_PRIVATE_KEY"),
-			},
-		],
-		from: {
-			email: Deno.env.get("SENDER_EMAIL"),
-			name: Deno.env.get("SENDER_NAME"),
-		},
-		subject: emailTemplate.subject,
-		content: [
-			{
-				type: "text/html",
-				value: content,
-			},
-		],
-	};
+	// const {
+	// 	email,
+	// 	type = "welcome",
+	// }: { email?: string; type?: keyof typeof emails } = request.params || {};
 
-	const res = await fetch("https://api.mailchannels.net/tx/v1/send", {
-		method: "POST",
-		body: JSON.stringify(emailPayload),
-		headers: {
-			"content-type": "application/json",
-		},
-	});
+	// if (!email) {
+	// 	return new Response("Invalid", { status: 404 });
+	// }
 
-	if (res.ok) {
-		return new Response("Email sent");
-	}
+	// const user = await getUserByEmail(email);
 
-	return new Response(
-		`Failed to send email. Got a ${res.status} from mailchennels.`,
-		{ status: 500 }
-	);
+	// if (!user) {
+	// 	return new Response("User not found", { status: 404 });
+	// }
+
+	// const token = await createUserToken(email, ["admin"], 7 * 24 * 60 * 60);
+
+	// const url = new URL(request.url);
+
+	// const emailTemplate = emails[type] || emails["sign-in"];
+
+	// const content = emailTemplate.content(
+	// 	`https://admin.crisislab.org.nz/token-sign-in?token=${token}&type=${type}${
+	// 		url.searchParams.get("return_to")
+	// 			? `&return_to=${url.searchParams.get("return_to")}`
+	// 			: ""
+	// 	}`,
+	// 	user.name
+	// );
+
+	// const emailPayload = {
+	// 	personalizations: [
+	// 		{
+	// 			to: [{ email, name: user.name }],
+	// 			dkim_domain: Deno.env.get("DKIM_DOMAIN"),
+	// 			dkim_selector: Deno.env.get(" DKIM_SELECTOR"),
+	// 			dkim_private_key: Deno.env.get(" DKIM_PRIVATE_KEY"),
+	// 		},
+	// 	],
+	// 	from: {
+	// 		email: Deno.env.get("SENDER_EMAIL"),
+	// 		name: Deno.env.get("SENDER_NAME"),
+	// 	},
+	// 	subject: emailTemplate.subject,
+	// 	content: [
+	// 		{
+	// 			type: "text/html",
+	// 			value: content,
+	// 		},
+	// 	],
+	// };
+
+	// const res = await fetch("https://api.mailchannels.net/tx/v1/send", {
+	// 	method: "POST",
+	// 	body: JSON.stringify(emailPayload),
+	// 	headers: {
+	// 		"content-type": "application/json",
+	// 	},
+	// });
+
+	// if (res.ok) {
+	// 	return new Response("Email sent");
+	// }
+
+	// return new Response(
+	// 	`Failed to send email. Got a ${res.status} from mailchennels.`,
+	// 	{ status: 500 }
+	// );
 }
--- src/api/sensors/sendSensorOfflineAlerts.ts
+++ src/api/sensors/sendSensorOfflineAlerts.ts
@@ -11,117 +11,119 @@ const globalRecipients: Record<string, string> = {
 const TwentyFourHoursMilliseconds = 24 * 60 * 60 * 1000;
 
 export async function sendSensorOfflineAlerts() {
-	const sensors = await getSensors();
+	// TODO: Re-implement email sending
+	return;
 
-	const recipientEmailAlertMap: Record<
-		string,
-		{ msg: string; sensorID: number }[]
-	> = {};
+	// 	const sensors = await getSensors();
 
-	const timeFormatter = new Intl.DateTimeFormat("en-NZ", {
-		dateStyle: "full",
-		timeStyle: "long",
-		timeZone: "Pacific/Auckland",
-	});
+	// 	const recipientEmailAlertMap: Record<
+	// 		string,
+	// 		{ msg: string; sensorID: number }[]
+	// 	> = {};
 
-	console.info("Generating offline alert summary emails...");
+	// 	const timeFormatter = new Intl.DateTimeFormat("en-NZ", {
+	// 		dateStyle: "full",
+	// 		timeStyle: "long",
+	// 		timeZone: "Pacific/Auckland",
+	// 	});
 
-	for (const sensor of Object.values(sensors)) {
-		if (sensor.online) continue;
+	// 	console.info("Generating offline alert summary emails...");
 
-		if (typeof sensor?.status_change_timestamp !== "number") continue;
+	// 	for (const sensor of Object.values(sensors)) {
+	// 		if (sensor.online) continue;
 
-		const lastPacketReceived = new Date(sensor.status_change_timestamp);
+	// 		if (typeof sensor?.status_change_timestamp !== "number") continue;
 
-		if (Date.now() - lastPacketReceived.getTime() < TwentyFourHoursMilliseconds)
-			continue;
+	// 		const lastPacketReceived = new Date(sensor.status_change_timestamp);
 
-		const message = `Sensor #${
-			sensor.id
-		} has not sent any data to the ingest server since ${formatRelative(
-			lastPacketReceived,
-			new Date(),
-			{ locale: enNZ }
-		)} which was ${formatDistance(lastPacketReceived, new Date(), {
-			locale: enNZ,
-			addSuffix: true,
-		})}.`;
+	// 		if (Date.now() - lastPacketReceived.getTime() < TwentyFourHoursMilliseconds)
+	// 			continue;
 
-		const alert = {
-			msg: message,
-			sensorID: sensor.id,
-		};
+	// 		const message = `Sensor #${
+	// 			sensor.id
+	// 		} has not sent any data to the ingest server since ${formatRelative(
+	// 			lastPacketReceived,
+	// 			new Date(),
+	// 			{ locale: enNZ }
+	// 		)} which was ${formatDistance(lastPacketReceived, new Date(), {
+	// 			locale: enNZ,
+	// 			addSuffix: true,
+	// 		})}.`;
 
-		const peopleToEmail = [...Object.keys(globalRecipients)];
-		if (sensor.contact_email) {
-			peopleToEmail.push(sensor.contact_email);
-		}
+	// 		const alert = {
+	// 			msg: message,
+	// 			sensorID: sensor.id,
+	// 		};
 
-		for (const email of peopleToEmail) {
-			if (!recipientEmailAlertMap[email]) recipientEmailAlertMap[email] = [];
+	// 		const peopleToEmail = [...Object.keys(globalRecipients)];
+	// 		if (sensor.contact_email) {
+	// 			peopleToEmail.push(sensor.contact_email);
+	// 		}
 
-			recipientEmailAlertMap[email].push(alert);
-		}
-	}
+	// 		for (const email of peopleToEmail) {
+	// 			if (!recipientEmailAlertMap[email]) recipientEmailAlertMap[email] = [];
 
-	for (const [email, alerts] of Object.entries(recipientEmailAlertMap)) {
-		const emailPayload = {
-			personalizations: [
-				{
-					to: [{ email, name: globalRecipients[email] || "Sensor Host" }],
-					dkim_domain: Deno.env.get("DKIM_DOMAIN"),
-					dkim_selector: Deno.env.get("DKIM_SELECTOR"),
-					dkim_private_key: Deno.env.get("DKIM_PRIVATE_KEY"),
-				},
-			],
-			from: {
-				email: Deno.env.get("SENDER_EMAIL"),
-				name: Deno.env.get("SENDER_NAME"),
-			},
-			subject: `Sensor status update (${alerts
-				.map(({ sensorID }) => `#${sensorID}`)
-				.join(", ")})`,
-			content: [
-				{
-					type: "text/plain",
-					value: `Hello ${email}!
-This is your CRISiSLab sensor status update digest email.
+	// 			recipientEmailAlertMap[email].push(alert);
+	// 		}
+	// 	}
 
-Here are the alerts relevant to you:
-${alerts.map(({ msg }) => `	- ${msg}`).join("\n")}
+	// 	for (const [email, alerts] of Object.entries(recipientEmailAlertMap)) {
+	// 		const emailPayload = {
+	// 			personalizations: [
+	// 				{
+	// 					to: [{ email, name: globalRecipients[email] || "Sensor Host" }],
+	// 					dkim_domain: Deno.env.get("DKIM_DOMAIN"),
+	// 					dkim_selector: Deno.env.get("DKIM_SELECTOR"),
+	// 					dkim_private_key: Deno.env.get("DKIM_PRIVATE_KEY"),
+	// 				},
+	// 			],
+	// 			from: {
+	// 				email: Deno.env.get("SENDER_EMAIL"),
+	// 				name: Deno.env.get("SENDER_NAME"),
+	// 			},
+	// 			subject: `Sensor status update (${alerts
+	// 				.map(({ sensorID }) => `#${sensorID}`)
+	// 				.join(", ")})`,
+	// 			content: [
+	// 				{
+	// 					type: "text/plain",
+	// 					value: `Hello ${email}!
+	// This is your CRISiSLab sensor status update digest email.
 
+	// Here are the alerts relevant to you:
+	// ${alerts.map(({ msg }) => `	- ${msg}`).join("\n")}
 
-This notification email was sent at ${timeFormatter.format(Date.now())}.
+	// This notification email was sent at ${timeFormatter.format(Date.now())}.
 
-If you aren't ${email}, please contact CRISiSLab technical staff.
+	// If you aren't ${email}, please contact CRISiSLab technical staff.
 
-If you have any questions or comments, just reply to this email and Zade will get back to you.`,
-				},
-			],
-		};
-		try {
-			const res = await fetch("https://api.mailchannels.net/tx/v1/send", {
-				method: "POST",
-				body: JSON.stringify(emailPayload),
-				headers: {
-					"content-type": "application/json",
-				},
-			});
-			const m = await res.text();
-			if (res.ok) {
-				console.info(`Sent offline alert summary email to ${email}`);
-			} else {
-				console.warn("Failed to send offline alert summary email:", email);
-				log.info(m, res);
-			}
+	// If you have any questions or comments, just reply to this email and Zade will get back to you.`,
+	// 				},
+	// 			],
+	// 		};
+	// 		try {
+	// 			const res = await fetch("https://api.mailchannels.net/tx/v1/send", {
+	// 				method: "POST",
+	// 				body: JSON.stringify(emailPayload),
+	// 				headers: {
+	// 					"content-type": "application/json",
+	// 				},
+	// 			});
+	// 			const m = await res.text();
+	// 			if (res.ok) {
+	// 				console.info(`Sent offline alert summary email to ${email}`);
+	// 			} else {
+	// 				console.warn("Failed to send offline alert summary email:", email);
+	// 				log.info(m, res);
+	// 			}
 
-			// TODO: New analytics
-			// EMAILS_SENT.writeDataPoint({
-			// 	doubles: [alerts.length],
-			// 	indexes: [email],
-			// });
-		} catch (err) {
-			console.warn("Failed to send email: ", err);
-		}
-	}
+	// 			// TODO: New analytics
+	// 			// EMAILS_SENT.writeDataPoint({
+	// 			// 	doubles: [alerts.length],
+	// 			// 	indexes: [email],
+	// 			// });
+	// 		} catch (err) {
+	// 			console.warn("Failed to send email: ", err);
+	// 		}
+	// 	}
 }
