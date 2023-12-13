31a6711e291c736cda62e95521f5ccc6f1e9cdf5
Zade Viggers <zade@viggers.net>
2023-12-14 12:32:04 +1300

    Remove email param

--- src/api/users/createUser.ts
+++ src/api/users/createUser.ts
@@ -2,26 +2,26 @@ import { IRequest, json } from "itty-router";
 import { validateEmail, getDB } from "../../utils.ts";
 
 export async function createUser(request: IRequest) {
-	const email = request.params.email.toLowerCase();
-
-	// check if email is valid
-	if (!validateEmail(email)) {
-		return new Response("Invalid", { status: 404 });
-	}
-
-	const sql = await getDB();
-
-	const emailUsed =
-		(await sql`SELECT count(*) FROM users WHERE email=${email};`)?.[0] ?? null;
-
-	if (emailUsed) return new Response("Email in use", { status: 400 });
-
 	const data = await request.json();
 	const userData = {
 		email: data.email,
 		name: data.name ?? "",
 		roles: data.roles ?? [],
 	};
+
+	// check if email is valid
+	if (!validateEmail(userData.email)) {
+		return new Response("Invalid", { status: 404 });
+	}
+
+	const sql = await getDB();
+
+	const emailUsed =
+		(
+			await sql`SELECT count(*) FROM users WHERE email=${userData.email};`
+		)?.[0] ?? null;
+
+	if (emailUsed) return new Response("Email in use", { status: 400 });
 
 	const id = (
 		await sql<{ id: number }[]>`INSERT INTO users ${sql(userData)} RETURNING id`
