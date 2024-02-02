76d609b957f9922282bba05aa3437327f7c53087
Zade Viggers <zade@viggers.net>
2024-02-02 14:30:18 +1300

    Revert "Revert "Explainer""
    This reverts commit 3866d5b22b1c059804da8b7e24fbfad71b130c6d.

--- src/utils.ts
+++ src/utils.ts
@@ -110,6 +110,7 @@ async function setupTables() {
 	`;
 }
 
+// Postgres.js manages connection pooling for us
 const sql = postgres({
 	user: Deno.env.get("DATABASE_USERNAME"),
 	password: Deno.env.get("DATABASE_PASSWORD"),
