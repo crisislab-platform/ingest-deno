1e17bf71e52aec9a6fd2a386d88a45d9887194bd
Zade Viggers <zade@viggers.net>
2024-02-03 16:43:58 +1300

    Re-enable size cron

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import {
 } from "https://deno.land/std@0.204.0/http/file_server.ts";
 import * as Sentry from "sentry";
 import { sensorHandler, handleWebSockets } from "./connectionHandler.ts";
-import { log } from "./utils.ts";
+import { getDB, log } from "./utils.ts";
 import { IRequest, Router } from "itty-router";
 import { handleAPI } from "./api/api.ts";
 loadENV({ export: true });
@@ -21,16 +21,16 @@ Sentry.init({
 });
 
 // Every hour, save DB size
-// Deno.cron("Save DB size", "0 */1 * * *", async () => {
-// 	const sql = await getDB();
-// 	try {
-// 		await sql`WITH subquery AS (SELECT pg_database_size('sensor_data') as size, NOW() as timestamp) INSERT INTO db_size_history (size, timestamp) SELECT * FROM subquery;`;
-// 	} catch (err) {
-// 		log.error("Error saving DB size:", err);
-// 	} finally {
-// 		log.info("Saved DB size");
-// 	}
-// });
+Deno.cron("Save DB size", "0 */1 * * *", async () => {
+	const sql = await getDB();
+	try {
+		await sql`WITH subquery AS (SELECT pg_database_size('sensor_data') as size, NOW() as timestamp) INSERT INTO db_size_history (size, timestamp) SELECT * FROM subquery;`;
+	} catch (err) {
+		log.error("Error saving DB size:", err);
+	} finally {
+		log.info("Saved DB size");
+	}
+});
 
 // Imports
 
